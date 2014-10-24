7e2c27fb2b536ddbaa85766c0b62420e8d941471
unknown <uilasil@comcast.net>
2014-10-24 02:02:31 +0000

    git hahaha

---
+++ app/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts._master')
+
+@section('title')
+Welcome to Developer's Best Friend
+@stop
+
+@section('content')
+<div class='container-fluid'>
+  <div class='well'>
+    <fieldset>
+      <h4>Lorem Ipsum Generator</h4>
+    </fieldset>
+  </div>
+</div>
+@stop
---
+++ app/views/layouts/_master.blade.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 1);
+?>
+<!DOCTYPE html>
+<html ng-app='App'>
+<head lang='en'>
+
+  <meta charset='UTF-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  
+  @section('javascript-libs')
+      <script src='//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js'></script>
+      <script src='//angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.11.0.js'></script>
+  @show
+
+  @section('css')
+    <link rel='stylesheet' href='{{ asset('css/app.css') }}'>
+    <link href='//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css' rel='stylesheet'>
+  @show
+
+  <title>@yield('title')</title>
+</head>
+<body>
+<div class='container-fluid'>
+
+  @include('layouts.header')
+
+  @yield('content')
+
+  @include('layouts.footer')
+
+</div>
+</body>
